7b09501efc279075c76a3582b1d2962b2065bcb5
Leonardo Dondoni <111018382+leonardodondoni@users.noreply.github.com>
2026-09-20 22:20:17 -0300

    feat: reach the instance through a CloudFront VPC origin (#5)
    Co-authored-by: Leonardo Dondoni <leonardo.siqueira@avel.me>

--- security.tf
+++ security.tf
@@ -1,12 +1,23 @@
-data "aws_ec2_managed_prefix_list" "cloudfront_origins" {
-  name = "com.amazonaws.global.cloudfront.origin-facing"
+resource "aws_cloudfront_vpc_origin" "api" {
+  vpc_origin_endpoint_config {
+    name                   = "seniors-api-origin"
+    arn                    = "arn:aws:ec2:${var.region}:${data.aws_caller_identity.current.account_id}:instance/${aws_instance.api.id}"
+    http_port              = 80
+    https_port             = 443
+    origin_protocol_policy = "http-only"
+
+    origin_ssl_protocols {
+      quantity = 1
+      items    = ["TLSv1.2"]
+    }
+  }
 }
 
 resource "aws_vpc_security_group_ingress_rule" "api_from_cloudfront" {
-  security_group_id = data.aws_security_group.api.id
-  description       = "CloudFront edge locations only"
-  ip_protocol       = "tcp"
-  from_port         = 80
-  to_port           = 80
-  prefix_list_id    = data.aws_ec2_managed_prefix_list.cloudfront_origins.id
+  security_group_id            = data.aws_security_group.api.id
+  description                  = "CloudFront VPC origin"
+  ip_protocol                  = "tcp"
+  from_port                    = 80
+  to_port                      = 80
+  referenced_security_group_id = "sg-08fa004f19113b6a5"
 }
